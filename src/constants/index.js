1fd4d44d566d063e1fea78af17354c1733469fbb
RESCALZO <escalzocamila@gmail.com>
2024-05-15 11:12:20 -0300

    Initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,317 @@
+import {
+    benefitIcon1,
+    benefitIcon2,
+    benefitIcon3,
+    benefitIcon4,
+    benefitIcon5,
+    benefitImage2,
+    chromecast,
+    disc02,
+    discord,
+    discordBlack,
+    facebook,
+    figma,
+    file02,
+    framer,
+    homeSmile,
+    instagram,
+    notification2,
+    notification3,
+    notification4,
+    notion,
+    photoshop,
+    plusSquare,
+    protopie,
+    raindrop,
+    recording01,
+    recording03,
+    roadmap1,
+    roadmap2,
+    roadmap3,
+    roadmap4,
+    searchMd,
+    slack,
+    sliders04,
+    telegram,
+    twitter,
+    yourlogo,
+  } from "../assets";
+  
+  export const navigation = [
+    {
+      id: "0",
+      title: "Features",
+      url: "#features",
+    },
+    {
+      id: "1",
+      title: "Pricing",
+      url: "#pricing",
+    },
+    {
+      id: "2",
+      title: "How to use",
+      url: "#how-to-use",
+    },
+    {
+      id: "3",
+      title: "Roadmap",
+      url: "#roadmap",
+    },
+  ];
+  
+  export const heroIcons = [homeSmile, file02, searchMd, plusSquare];
+  
+  export const notificationImages = [notification4, notification3, notification2];
+  
+  export const companyLogos = [yourlogo, yourlogo, yourlogo, yourlogo, yourlogo];
+  
+  export const brainwaveServices = [
+    "Assessment & Setup",
+    "Data Entry & Organization",
+    "Review & Optimization",
+  ];
+  
+  export const brainwaveServicesIcons = [
+    recording03,
+    recording01,
+    disc02,
+    chromecast,
+    sliders04,
+  ];
+  
+  export const roadmap = [
+    {
+      id: "0",
+      title: "Periodic Updates",
+      text: "Keep in the know with our Periodic Updates, delivering timely financial insights tailored to your needs.",
+      date: "May 2023",
+      status: "done",
+      imageUrl: roadmap1,
+      colorful: true,
+    },
+    {
+      id: "1",
+      title: "Gamification",
+      text: "Add game-like elements, such as badges or leaderboards, to incentivize users to engage with the chatbot more frequently.",
+      date: "May 2023",
+      status: "progress",
+      imageUrl: roadmap2,
+    },
+    {
+      id: "2",
+      title: "Chatbot customization",
+      text: "Allow users to customize the chatbot's appearance and behavior, making it more engaging and fun to interact with.",
+      date: "May 2023",
+      status: "done",
+      imageUrl: roadmap3,
+    },
+    {
+      id: "3",
+      title: "Integration with APIs",
+      text: "Allow the chatbot to access external data sources, such as weather APIs or news APIs, to provide more relevant recommendations.",
+      date: "May 2023",
+      status: "progress",
+      imageUrl: roadmap4,
+    },
+  ];
+  
+  export const collabText =
+    "Bookkeeping transforms chaos into clarity, empowering your financial journey with insights that fuel growth and success.";
+  
+  export const collabContent = [
+    {
+      id: "0",
+      title: "Trust",
+      text: collabText,
+    },
+    {
+      id: "1",
+      title: "Insight",
+    },
+    {
+      id: "2",
+      title: "Accuracy",
+    },
+  ];
+  
+  export const collabApps = [
+    {
+      id: "0",
+      title: "Figma",
+      icon: figma,
+      width: 36,
+      height: 36,
+    },
+    {
+      id: "1",
+      title: "Notion",
+      icon: notion,
+      width: 40,
+      height: 50,
+    },
+    {
+      id: "2",
+      title: "Discord",
+      icon: discord,
+      width: 36,
+      height: 28,
+    },
+    {
+      id: "3",
+      title: "Slack",
+      icon: slack,
+      width: 40,
+      height: 35,
+    },
+    {
+      id: "4",
+      title: "Photoshop",
+      icon: photoshop,
+      width: 34,
+      height: 34,
+    },
+    {
+      id: "5",
+      title: "Protopie",
+      icon: protopie,
+      width: 34,
+      height: 34,
+    },
+    {
+      id: "6",
+      title: "Framer",
+      icon: framer,
+      width: 36,
+      height: 34,
+    },
+    {
+      id: "7",
+      title: "Raindrop",
+      icon: raindrop,
+      width: 40,
+      height: 32,
+    },
+  ];
+  
+  export const pricing = [
+    {
+      id: "0",
+      title: "Basic",
+      description: "AI chatbot, personalized recommendations",
+      price: "0",
+      features: [
+        "...",
+        "...",
+        "...",
+      ],
+    },
+    {
+      id: "1",
+      title: "Premium",
+      description: "Advanced AI chatbot, priority support, analytics dashboard",
+      price: "9.99",
+      features: [
+        "...",
+        "...",
+        "...",
+      ],
+    },
+    {
+      id: "2",
+      title: "Enterprise",
+      description: "Custom AI chatbot, advanced analytics, dedicated account",
+      price: null,
+      features: [
+        "...",
+        "...",
+        "...",
+      ],
+    },
+  ];
+  
+  export const benefits = [
+    {
+      id: "0",
+      title: "Bookkeeping",
+      text: "Keep your focus on growth while we handle the books. Our expert bookkeeping ensures accuracy and compliance.",
+      backgroundUrl: "assets/benefits/card-1.svg",
+      iconUrl: benefitIcon1,
+      imageUrl: benefitImage2,
+    },
+    {
+      id: "1",
+      title: "Monthly reporting",
+      text: "Stay informed effortlessly. Our monthly reporting delivers clear insights for informed decision-making.",
+      backgroundUrl: "assets/benefits/card-2.svg",
+      iconUrl: benefitIcon2,
+      imageUrl: benefitImage2,
+      light: true,
+    },
+    {
+      id: "2",
+      title: "Accounts reconciliation",
+      text: "Ensure financial accuracy. Our accounts reconciliation service guarantees precision and peace of mind.",
+      backgroundUrl: "assets/benefits/card-3.svg",
+      iconUrl: benefitIcon3,
+      imageUrl: benefitImage2,
+    },
+    {
+      id: "3",
+      title: "Accounts payables",
+      text: "Simplify your payables. Our accounts payable service streamlines payments, ensuring efficiency and control.",
+      backgroundUrl: "assets/benefits/card-4.svg",
+      iconUrl: benefitIcon4,
+      imageUrl: benefitImage2,
+      light: true,
+    },
+    {
+      id: "4",
+      title: "Accounts receivables",
+      text: "Accelerate your receivables. Our accounts receivable service maximizes cash flow with seamless invoicing and timely collections.",
+      backgroundUrl: "assets/benefits/card-5.svg",
+      iconUrl: benefitIcon5,
+      imageUrl: benefitImage2,
+    },
+    {
+      id: "5",
+      title: "Tax Prep",
+      text: "Benefits of getting prepared early for tax season.",
+      backgroundUrl: "assets/benefits/card-6.svg",
+      iconUrl: benefitIcon2,
+      imageUrl: benefitImage2,
+    },
+  ];
+  
+  export const socials = [
+    {
+      id: "0",
+      title: "Discord",
+      iconUrl: discordBlack,
+      url: "#",
+    },
+    {
+      id: "1",
+      title: "Twitter",
+      iconUrl: twitter,
+      url: "#",
+    },
+    {
+      id: "2",
+      title: "Instagram",
+      iconUrl: instagram,
+      url: "#",
+    },
+    {
+      id: "3",
+      title: "Telegram",
+      iconUrl: telegram,
+      url: "#",
+    },
+    {
+      id: "4",
+      title: "Facebook",
+      iconUrl: facebook,
+      url: "#",
+    },
+  ];
